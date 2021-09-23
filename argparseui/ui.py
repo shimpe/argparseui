1d17d11f782dd82c2a9e43e30ba0ff1648eb093e
Graham <4gra@users.noreply.github.com>
2021-09-23 21:03:04 +0100

    remove redundant imports
    no need to reimport at the __main__ section, and slimmed sys to argv

--- argparseui/ui.py
+++ argparseui/ui.py
@@ -760,8 +760,7 @@ class ArgparseUi(QtGui.QDialog):
 
 
 if __name__ == "__main__":
-    import sys
-    from PyQt5 import QtWidgets as QtGui
+    from sys import argv
 
     # EXPERIMENT USING BASIC PARSER
     parser = argparse.ArgumentParser()
@@ -777,7 +776,7 @@ if __name__ == "__main__":
     group.add_argument("-q", "--quiet", action="store_true")
     parser.add_argument('--foo', type=int, nargs='+')
     parser.add_argument('--bar', type=int, nargs=2, metavar=('bar', 'baz'))
-    app = QtGui.QApplication(sys.argv)
+    app = QtGui.QApplication(argv)
     a = ArgparseUi(parser, left_label_alignment=True, use_scrollbars=True, use_save_load_button=True)
     a.show()
     app.exec_()
